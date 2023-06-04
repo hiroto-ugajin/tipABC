f0a71f1ad1e1733eef4bc39954e0e5d898fd023a
hiroto-ugajin <hrtugauga0324@gmail.com>
2023-06-04 17:03:08 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/hiroto/ugajin/tipabc/MainActivity.kt
@@ -0,0 +1,26 @@
+package jp.techacademy.hiroto.ugajin.tipabc
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import jp.techacademy.hiroto.ugajin.tipabc.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.button.setOnClickListener(this)
+    }
+    override fun onClick(v: View?) {
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/jp/techacademy/hiroto/ugajin/tipabc/SecondActivity.kt
@@ -0,0 +1,11 @@
+package jp.techacademy.hiroto.ugajin.tipabc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
